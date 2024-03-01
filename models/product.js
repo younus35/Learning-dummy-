471283241e6f08222254d21db4cf21c53524ae6b
younus35 <younuscode35@gmail.com>
2024-03-01 23:33:15 +0530

    models in mvc

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(__dirname, "../", 
+"data",
+"products.json");
+
+const getProductsFromFile = cb =>{
+     fs.readFile(p, (err,data) =>{
+         if(err){
+            cb([]);
+        }else{
+         cb(JSON.parse(data));
+        }
+     });
+}
+
+module.exports = class Product {
+    constructor(t){
+        this.title = t;
+    }
+    save() {
+        getProductsFromFile(products =>{
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err)=>{
+            console.log(err);
+            });
+        });
+    }
+    static fetchAll(cb) {//directlly calls on the class itself not on obj
+        getProductsFromFile(cb);
+    }
+}
