680833171e892557c4d9f38a2a55c5fa901375be
younus35 <younuscode35@gmail.com>
2024-03-08 17:37:13 +0530

    user and product one to many relationship

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,14 +14,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct({
+  req.user
+  .createProduct({
     title: title,//right side reffering to const and left one of the attribute in the model
     price: price,
     imageUrl: imageUrl,
     description: description
     // userId: req.user.id
-  });
-  Product.create()
+  })
+  // Product.create()
   .then(result =>{
     // console.log(result);
     res.redirect('/admin/products');
@@ -35,8 +36,11 @@ exports.getEditProduct = (req, res, next) => {
    return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product =>{
+  req.user
+    .getProducts({ where: { id: prodId } })
+  // Product.findByPk(prodId)
+  .then(products =>{
+    const product= products[0];
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
@@ -82,7 +86,9 @@ exports.deleteProduct = (req, res, next) =>{
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll()
+  req.user
+  .getProducts()
+    // Product.findAll()
     .then((products) => {
       res.render('admin/products', {
         prods: products,
